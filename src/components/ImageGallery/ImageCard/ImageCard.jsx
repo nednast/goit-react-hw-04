c0620fac516b0df6b2201636579397bdac428b4a
NNASTYA2007 <nedyaknast@gmail.com>
2024-04-07 17:19:42 +0200

    Update ImageCard.jsx

--- src/components/ImageGallery/ImageCard/ImageCard.jsx
+++ src/components/ImageGallery/ImageCard/ImageCard.jsx
@@ -1,18 +1,16 @@
 const ImageCard = ({ image, onSelect }) => {
   return (
     <div>
-      <div>
-        <img
-          src="{image.urls.small}"
-          alt="{image.alt_description}"
-          onClick={() =>
-            onSelect(true, {
-              src: image.urls.regular,
-              description: image.alt_description,
-            })
-          }
-        />
-      </div>
+      <img
+        src="{image.urls.small}"
+        alt="{image.alt_description}"
+        onClick={() =>
+          onSelect(true, {
+            src: image.urls.regular,
+            description: image.alt_description,
+          })
+        }
+      />
     </div>
   );
 };
